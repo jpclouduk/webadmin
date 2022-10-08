2dc51f874f3aebc2b7c02659118a6c26626459e0
jpclouduk <poulsenjx@gmail.com>
2022-10-08 13:13:46 +0100

    add promotion script

---
+++ promote.bash
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#
+############################################
+# Script:  Push build code to apache docs  #
+# Author:  JP                              #
+# Date: 08/10/2022                         #
+############################################
+
+
+# VARS
+base=/opt
+site=website
+docs=/var/www/html
+
+# Promote build file to document root
+/usr/bin/cp -r $base/$site/build/* $docs/
