1d956872de497c2340e776fa21f1d71ed04170fe
jpclouduk <poulsenjx@gmail.com>
2022-10-08 08:52:10 +0100

    Update rebuild.bash

--- rebuild.bash
+++ rebuild.bash
@@ -14,16 +14,20 @@ user=jpclouduk
 pass=`cat /opt/token`
 
 # Clear website
+echo "#### Removing Website #### 
 /usr/bin/rm -rf $base/$site
 
 # Pull down site from github
+echo "#### Retrieving Website From Github #### 
 cd $base
 /usr/bin/git clone https://$user:$pass@github.com/jpclouduk/website.git
 
 # Merge code with Docusaurus
+echo "#### Merging Docusaurus #### 
 cd $base/$site
 /ur/bin/npm install
 
-#Build website
+# Build website
+echo "#### Building Website #### 
 cd $base/$site
 /usr/bin/npm run build
