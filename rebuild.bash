e37107f5b9bff2b45cf8eb42a905dc292fc58ddc
jpclouduk <poulsenjx@gmail.com>
2022-10-08 08:32:15 +0100

    first commit

---
+++ rebuild.bash
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+#
+##########################################
+# Script:  Rebuild Docusaurus            #
+# Author:  JP                            #
+# Date: 08/10/2022                       #
+##########################################
+
+
+# VARS
+repo=/opt/docus
+site=jpclouduk
+
+# 
