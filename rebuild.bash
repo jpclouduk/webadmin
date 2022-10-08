ab2d1b5ff2b9b50281e1c1efa7c8f646ed052d65
jpclouduk <poulsenjx@gmail.com>
2022-10-08 08:41:24 +0100

    Update rebuild.bash

--- rebuild.bash
+++ rebuild.bash
@@ -8,7 +8,21 @@
 
 
 # VARS
-repo=/opt/docus
-site=jpclouduk
+base=/opt
+site=website
+pass=`cat /opt/token`
 
-# 
+# Clear website
+/usr/bin/rm $base/$site
+
+# Pull down site from github
+cd $base
+git clone https://github.com/jpclouduk/website.git
+
+# Merge code with Docusaurus
+cd $base/$site
+npm install
+
+#Build website
+cd $base/$site
+npm run build
