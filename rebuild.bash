a37525e4d3c7ad531c77588b74bcd2d7bc0cb173
jpclouduk <poulsenjx@gmail.com>
2022-10-13 19:21:21 +0100

    Update rebuild.bash

--- rebuild.bash
+++ rebuild.bash
@@ -12,6 +12,7 @@ base=/opt
 site=website
 user=jpclouduk
 pass=`cat /opt/token`
+lina=`arch`
 
 # Check command line
 if [[ $1 == promote ]]
@@ -21,10 +22,23 @@ else
 	echo "!!!!!  No deployment to production has been selected  !!!!!"
 fi
 
+# Setup architecture options
+case $lina in
+    x86_64)
+        binp=/usr/bin
+	;;
+    armv7l)
+        binp=/bin
+	;;
+    *)
+        printf "Unknown machine type\n Not armv7l or x86_64\n Use uname -m to check type.\n"
+	;;
+esac
+
 
 # Clear website
-echo "#### Removing Website ####"
-/usr/bin/rm -rf $base/$site
+$binp/echo "#### Removing Website ####"
+$binp/rm -rf $base/$site
 
 # Pull down site from github
 echo "#### Retrieving Website From Github ####"
@@ -32,12 +46,12 @@ cd $base
 /usr/bin/git clone https://$user:$pass@github.com/jpclouduk/website.git
 
 # Merge code with Docusaurus
-echo "#### Merging Docusaurus ####"
+$binp/echo "#### Merging Docusaurus ####"
 cd $base/$site
 /usr/bin/npm install
 
 # Build website
-echo "#### Building Website ####"
+$binp/echo "#### Building Website ####"
 cd $base/$site
 /usr/bin/npm run build
 
